02b1eefb6b6ef7b3b3c14947bdbc3404822241e3
son66 <son66@DESKTOP-A9TSDTP>
2021-04-11 23:05:23 +0900

    first commit

---
+++ GaemandaReact/src/main/java/com/example/demo/GaemandaReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaemandaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaemandaReactApplication.class, args);
+	}
+
+}
